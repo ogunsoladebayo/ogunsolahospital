8d7159d16aa667eafbe82b7497b189cf92e6bc22
ogunsola-activator <ogunsoladeabayo@gmail.com>
2020-05-04 02:27:16 +0100

    implemented payment

---
+++ flutterwave/transfunc.php
@@ -0,0 +1,17 @@
+<?php
+
+function save_transaction($transactionData){
+    $transaction = json_decode(file_get_contents("transactionData/" . $transactionData->custemail . ".json"));
+    $transaction[] = Array(
+        'TransactionID' => $transactionData-> txid,
+        'TransactionRef' => $transactionData-> txref,
+        'Date' => date('l, d-m-Y'),
+        'Time' => date('h:i:sa'),
+        'Amount' => $transactionData->amount,
+        'Currency' => $transactionData->currency,
+        'Narration' => $transactionData->narration,
+        'Status' => $transactionData->status
+    );
+    file_put_contents("transactionData/" . $transactionData->custemail . ".json", json_encode($transaction));
+
+}
